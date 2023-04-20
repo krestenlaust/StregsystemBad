643f434f342f8941eda6779e4ee1e3a1b758c9d5
Kresten Laust <krestenlaust@gmail.com>
2023-04-20 19:45:16 +0200

    Partially added validating

---
+++ Stregsystem.Core/DataValidator.cs
@@ -0,0 +1,32 @@
+﻿using Stregsystem.Core.Validator;
+
+namespace Stregsystem.Core
+{
+    public class DataValidator : IUsernameValidator, INameValidator, IEmailValidator
+    {
+        readonly string UsernameRegex = "";
+        readonly string FirstnameRegex = "";
+        readonly string SurnameRegex = "";
+        readonly string EmailRegex = "";
+
+        public bool IsEmailValid(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsFirstnameValid(string firstname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSurnameValid(string surname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUsernameValid(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Stregsystem.Core/Validator/IUsernameValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stregsystem.Core.Validator
+{
+    public interface IUsernameValidator
+    {
+        public bool IsUsernameValid(string username);
+    }
+}
